a6d68bd3b23d79d196dafc7e3bf2af8d78df9663
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-29 19:47:14 +0900

    update main
    storyboardにて基本UIを構築し、ViewControllerと紐付け

--- YouTubeApp/ViewController.swift
+++ YouTubeApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  YouTubeApp
 //
 //  Created by 佐々木　謙 on 2021/03/29.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
