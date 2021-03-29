535cde275f220e3f434b0f9d570946f51154fcf2
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-29 19:23:30 +0900

    Initial Commit

---
+++ YouTubeAppTests/YouTubeAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  YouTubeAppTests.swift
+//  YouTubeAppTests
+//
+//  Created by 佐々木　謙 on 2021/03/29.
+//
+
+import XCTest
+@testable import YouTubeApp
+
+class YouTubeAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
