aa95bf1d2b6d311fb61b04485458ceea8d392508
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-03-30 16:07:02 +0900

    update main
    検索結果（レスポンス）を保管するために構造体を作成

---
+++ YouTubeApp/VideoItems.swift
@@ -0,0 +1,23 @@
+//
+//  VideoItems.swift
+//  YouTubeApp
+//
+//  Created by 佐々木　謙 on 2021/03/30.
+//
+
+import Foundation
+
+// レスポンス結果の階層を扱う構造体
+struct VideoItems {
+    
+    var title       : String
+    var channelTitle: String
+    var thumbnailURL: String
+    
+    init(videTitle: String, videoChannel: String, videoImage: String) {
+        
+        title        = videTitle
+        channelTitle = videoChannel
+        thumbnailURL = videoImage
+    }
+}
